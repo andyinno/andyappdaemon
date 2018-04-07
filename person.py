9eba2c55d7fdfb832260e7731001882daf8e1ae7
Andrea Innocenti <andyinno@gmail.com>
2018-04-07 20:06:12 +0200

    Added timer for evaluating away/home status

--- person.py
+++ person.py
@@ -1,4 +1,5 @@
 import appdaemon.plugins.hass.hassapi as hass
+from datetime import timedelta
 
 class Person(hass.Hass):
     def initialize(self):
@@ -12,6 +13,9 @@ class Person(hass.Hass):
         self._peripheral_sensors = self.args.get("peripheral_sensors", None)
         self._pending_notification = False
         self._back_home = False
+        self._timer = None
+        self._person_time = self.datetime()
+        self._family_time = self.datetime()
 
         self.log("Got name {}".format(self._name))
         self.log("Got trackers {}".format(self._tracker))
@@ -26,8 +30,12 @@ class Person(hass.Hass):
         self.listen_state(self.person_changed_home, entity=self._tracker, old="not_home", new="home")
         self.listen_state(self.door_opened, entity=self._door_sensor, new="off")
 
+
     def family_changed_away(self, entity, attribute, old, new, kwargs):
         self.log("Family state changed to away")
+        self._family_time = self.datetime()
+        if self._timer is not None:
+            self.cancel_timer(self._timer)
 
     def family_changed_home(self, entity, attribute, old, new, kwargs):
         self.log("Family state changed to home")
@@ -36,19 +44,27 @@ class Person(hass.Hass):
         self.log("{} state changed to home".format(self._name))
         self._back_home = True
 
-
     def person_changed_away(self, entity, attribute, old, new, kwargs):
         self.log("{} state changed to away".format(self._name))
+        self._person_time = self.datetime()
         if self._pending_notification == True and self.get_state(self._family_tracker) == "not_home":
-            self.notify("Molto probabilmente delle finestre sono rimaste aperte.", title="Avviso finestre",
-                        name=self._notifier)
-        self._pending_notification = False
-        self._back_home = False
+            if self._timer is not None:
+                self.cancel_timer(self._timer)
+            self._timer = self.run_in(self.send_pending_notification, 60)
 
     def door_opened(self, entity, attribute, old, new, kwargs):
         self.log("{} opened".format(self._door_sensor))
+        if self._timer is not None:
+            self.cancel_timer(self._timer)
         if (self.get_state(self._tracker) == "home"):
             self._pending_notification = True
         if self._back_home:
             self.notify("Ciao {}! Come procede la giornata? ".format(self._name), name=self._notifier)
+        self._back_home = False
+
+    def send_pending_notification(self, kwargs):
+        if (self._family_time - self._person_time).total_seconds() < 120:
+            self.notify("Molto probabilmente delle finestre sono rimaste aperte.", title="Avviso finestre",
+                        name=self._notifier)
+        self._pending_notification = False
         self._back_home = False
