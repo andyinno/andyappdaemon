e483fc37059b8bd26bfa692c6865cad8ef6638c2
Andrea Innocenti <andyinno@gmail.com>
2018-04-11 23:00:56 +0200

    Added illumination management

--- home_management.py
+++ home_management.py
@@ -6,6 +6,8 @@ class Room(hass.Hass):
     def initialize(self):
         self._name = self.args.get("name", None)
         self._motion_sensor = self.args.get("motion_sensor", None)
+        self._illumination = self.args.get("illumination", None)
+        self._max_ill = self.args.get("max_illumination", 999)
         self._light = self.args.get("light", None)
         self._flux = self.args.get("flux", None)
 
@@ -16,12 +18,19 @@ class Room(hass.Hass):
         self.log("Got motion_sensor {}".format(self._motion_sensor))
         self.log("Got light {}".format(self._light))
         self.log("Got flux {}".format(self._flux))
+        self.log("Got illumination {}".format(self._illumination))
+        self.log("Got limit {}".format(self._max_ill))
 
     def motion(self, entity, attribute, old, new, kwargs):
         self.log("{} detected motion".format(self._name))
         self.turn_on(self._light)
         if self._timer is not None:
             self.cancel_timer(self._timer)
+        if self._illumination is not None:
+            if self.get_state(self._illumination) > self._max_ill:
+                self.log("Room {} is too bright.".format(self._name))
+                return
+            
         self._timer = self.run_in(self.demotion, 300)
         if self._flux is not None:
             self.call_service(self._flux)
@@ -40,4 +49,3 @@ class BedRoomNight(Room):
             self.cancel_timer(self._timer)
         self._timer = self.run_in(self.demotion, 300)
 
-        
