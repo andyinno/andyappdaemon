bfd26ac55daf352493d371d6c8dd3b10d00e57f9
Andrea Innocenti <andyinno@gmail.com>
2018-04-11 23:54:59 +0200

    Moved timer before possible return

--- home_management.py
+++ home_management.py
@@ -21,18 +21,18 @@ class Room(hass.Hass):
         self.log("Got illumination {}".format(self._illumination))
         self.log("Got limit {}".format(self._max_ill))
         self._timer = self.run_in(self.demotion, 300)
-        
+
     def motion(self, entity, attribute, old, new, kwargs):
         self.log("{} detected motion".format(self._name))
         self.turn_on(self._light)
         if self._timer is not None:
             self.cancel_timer(self._timer)
+        self._timer = self.run_in(self.demotion, 300)
         if self._illumination is not None:
             if self.get_state(self._illumination) > self._max_ill:
                 self.log("Room {} is too bright.".format(self._name))
                 return
 
-        self._timer = self.run_in(self.demotion, 300)
         if self._flux is not None:
             self.call_service(self._flux)
 
