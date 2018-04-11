b4f0469b9f50e9bbdad1d9a1d6ca7727f07eb442
Andrea Innocenti <andyinno@gmail.com>
2018-04-11 22:51:46 +0200

    Added new light module as home_management

---
+++ home_management.py
@@ -0,0 +1,43 @@
+import appdaemon.plugins.hass.hassapi as hass
+
+import datetime
+
+class Room(hass.Hass):
+    def initialize(self):
+        self._name = self.args.get("name", None)
+        self._motion_sensor = self.args.get("motion_sensor", None)
+        self._light = self.args.get("light", None)
+        self._flux = self.args.get("flux", None)
+
+        self.listen_state(self.motion, entity=self._motion_sensor, new='on')
+        self._timer = None
+
+        self.log("Got name {}".format(self._name))
+        self.log("Got motion_sensor {}".format(self._motion_sensor))
+        self.log("Got light {}".format(self._light))
+        self.log("Got flux {}".format(self._flux))
+
+    def motion(self, entity, attribute, old, new, kwargs):
+        self.log("{} detected motion".format(self._name))
+        self.turn_on(self._light)
+        if self._timer is not None:
+            self.cancel_timer(self._timer)
+        self._timer = self.run_in(self.demotion, 300)
+        if self._flux is not None:
+            self.call_service(self._flux)
+
+    def demotion(self, kwargs):
+        self.log("{}  timer ended".format(self._name))
+        self.turn_off(self._light)
+        if (self.get_state(self._light)=='on'):
+            self._timer = self.run_in(self.demotion, 10)
+
+class BedRoomNight(Room):
+    def motion(self, entity, attribute, old, new, kwargs):
+        self.log("{} detected motion by night".format(self._name))
+        self.turn_on(self._light, color_name="red", brightness="30")
+        if self._timer is not None:
+            self.cancel_timer(self._timer)
+        self._timer = self.run_in(self.demotion, 300)
+
+        
