967c1182fe48b3a8476df32dbfff042613a4ea51
Andrea Innocenti <andyinno@gmail.com>
2018-04-11 23:14:02 +0200

    Be sure that the lights turns off on init if there is no movement

--- home_management.py
+++ home_management.py
@@ -20,7 +20,8 @@ class Room(hass.Hass):
         self.log("Got flux {}".format(self._flux))
         self.log("Got illumination {}".format(self._illumination))
         self.log("Got limit {}".format(self._max_ill))
-
+        self._timer = self.run_in(self.demotion, 300)
+        
     def motion(self, entity, attribute, old, new, kwargs):
         self.log("{} detected motion".format(self._name))
         self.turn_on(self._light)
@@ -30,7 +31,7 @@ class Room(hass.Hass):
             if self.get_state(self._illumination) > self._max_ill:
                 self.log("Room {} is too bright.".format(self._name))
                 return
-            
+
         self._timer = self.run_in(self.demotion, 300)
         if self._flux is not None:
             self.call_service(self._flux)
